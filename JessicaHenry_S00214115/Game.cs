9f44d4dd9b2c41d7d86bd3489987c12a312fb743
Jessica Henry <S00214115@mail.itsligo.ie>
2022-05-08 23:41:10 +0100

    2 => Create Class Game

---
+++ JessicaHenry_S00214115/Game.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JessicaHenry_S00214115
+{
+    public class Game
+    {
+       string Name { get; set; }
+       decimal MetacriticScore { get; set; }
+       string Description { get; set; }
+       string Platform { get; set; }
+       decimal Price { get; set; }
+       string Game_Image { get; set; }
+        
+    }
+}
