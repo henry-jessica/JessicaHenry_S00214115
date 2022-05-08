e645d7f2845bad2cd3bc81a94e39bbd148293f6f
Jessica Henry <S00214115@mail.itsligo.ie>
2022-05-09 00:12:46 +0100

    4=> create  CodeFirst Entity Framework

---
+++ DataManagement/Program.cs
@@ -0,0 +1,59 @@
+﻿using JessicaHenry_S00214115;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataManagement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //add reference to database
+            GameData db = new GameData();
+
+            using (db)
+            {
+                Game g1 = new Game()
+                {
+                    Name = "It Takes Two",
+                    Platform = "PC, Xbox, PS, Switch",
+                    MetacriticScore = 88.8m,
+                    Price = 69.99m,
+                    Description = "Description of the game"
+                };
+                Game g2 = new Game()
+                {
+                    Name = "Resident Evil",
+                    Platform = "PC, Xbox, Switch",
+                    MetacriticScore = 98.8m,
+                    Price = 169.99m,
+                    Description = "Resident and Evil"
+                };
+                Game g3 = new Game()
+                {
+                    Name = "Miles Morales",
+                    Platform = "PS",
+                    MetacriticScore = 75.8m,
+                    Price = 149.99m,
+                    Description = "Description of the game Miles Morales"
+                };
+
+                Console.WriteLine("Games Have been created");
+
+                db.Games.Add(g1);
+                db.Games.Add(g1);
+                db.Games.Add(g1);
+                db.Games.Add(g1);
+
+                Console.WriteLine("Games have been added to table");
+
+                db.SaveChanges();
+                Console.WriteLine("Data has been saved to DB, hit enter to continue");
+                Console.ReadLine(); 
+            }
+        }
+    }
+}
--- JessicaHenry_S00214115/Game.cs
+++ JessicaHenry_S00214115/Game.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,8 @@ namespace JessicaHenry_S00214115
 {
     public class Game
     {
+        //properties 
+        
        public string Name { get; set; }
        public decimal MetacriticScore { get; set; }
        public string Description { get; set; }
@@ -15,9 +18,19 @@ namespace JessicaHenry_S00214115
        public decimal Price { get; set; }
        public string Game_Image { get; set; }
      
+        //constructors 
+
+        //methods
         public void DecreasePrice(decimal amountToDecrease)
         {
             Price -= amountToDecrease;
         }
+    }//end of Game class
+
+    public class GameData : DbContext
+    {
+        public GameData() : base("MyGameData0001") { }
+        public DbSet<Game> Games { get; set; }
+
     }
 }
